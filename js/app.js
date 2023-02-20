a73e78a70ad41f920f8619813fedeeec39c881d4
albertopiffer <albertopiffer96@gmail.com>
2023-02-20 18:40:08 +0100

    FIX corretto errore nel css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//creo l'array
+//do un id al body e gli assegno position relative
+//con un for aggiungo le immagini dell'array nell'html e gli assegno display none
+//creo una variabile active che controllerà quale immagine viene visualizzata
+//creo il markup delle frecce
+//creo le funzioni avanti e indietro che al click delle frecce modificano la variabile active e il display delle immagini da visualizzare/nascondere
+// BONUS con un if su active rendo possibie il loop delle immagini
+
 const slides = [
     './img/01.jpg',
     './img/02.jpg',
